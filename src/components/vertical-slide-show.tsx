86fabe1053e57c0816726dde947ac737c2258056
ioetbc <ioetbc@gmail.com>
2024-03-30 22:49:02 +0000

    capture slideshow change

--- src/components/vertical-slide-show.tsx
+++ src/components/vertical-slide-show.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from "react";
-import {Button, StyleSheet, View} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {Image} from "expo-image";
 import {HERO_IMAGE_HEIGHT} from "@/consts";
 import Carousel from "react-native-reanimated-carousel";
@@ -22,6 +22,7 @@ export const VerticalSlideShow = ({images, activeIndex}: Props) => {
       <Carousel
         ref={ref}
         data={images}
+        onSnapToItem={(index) => console.log("onSnapToItem", index)}
         renderItem={(item) => {
           console.log("item", item);
           return (
@@ -43,7 +44,6 @@ export const VerticalSlideShow = ({images, activeIndex}: Props) => {
 
 const styles = StyleSheet.create({
   image: {
-    // flex: 1,
     height: HERO_IMAGE_HEIGHT,
     backgroundColor: "red",
   },
